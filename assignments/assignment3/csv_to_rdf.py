e5724e4b97840b54b109a13588b91afb7c7524f6
Luca Modica <luca.modica55@gmail.com>
2024-04-26 00:51:50 +0200

    ✨ feat(CSV TO RDF): function for conversion

--- assignments/assignment3/csv_to_rdf.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-assigned_hours = pd.read_csv('path/to/assigned_hours.csv')
-reported_hours = pd.read_csv('path/to/reported_hours.csv')
-
-teacher_hours = assigned_hours.merge(reported_hours, on=['Course code', 'Teacher Id'], how='left')
-teacher_hours['hoursStatus'] = ~teacher_hours['Reported hours'].isnull()
-teacher_hours.drop('Reported hours', axis=1, inplace=True)
